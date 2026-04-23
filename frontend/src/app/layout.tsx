ba761849015a1b38759cced535153403cd34826f
Silas Jimmy <silas.jimmy@softclans.co.ke>
2026-04-23 15:47:46 +0300

    configure: remove mantine from next-js frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,30 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import "@mantine/core/styles.css";
-
-import {
-  ColorSchemeScript,
-  MantineProvider,
-  mantineHtmlProps,
-} from "@mantine/core";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Todo App",
-  description: "Track your schedule on a timely fashion",
+  title: "Todo app",
+  description: "Track your todos with ease",
 };
 
 export default function RootLayout({
@@ -33,18 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      {...mantineHtmlProps}
-    >
-      <head>
-        <ColorSchemeScript />
-      </head>
-      
-      <body className="min-h-full flex flex-col">
-        <MantineProvider>{children}</MantineProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1>Hello!</h1>
+    <div className="border">
+      <h1>Home</h1>
+    </div>
   );
 }

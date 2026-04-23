9c6cbd145c65cc34c29dc5bd96ac652336c8c62a
Silas Jimmy <silas.jimmy@softclans.co.ke>
2026-04-23 20:47:37 +0300

    refactor: create login form

--- frontend/src/app/auth/sign-in/page.tsx
+++ frontend/src/app/auth/sign-in/page.tsx
@@ -1,24 +1,69 @@
 "use client";
 
-import { Button, Card, PasswordInput, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import {
+  Button,
+  Card,
+  Checkbox,
+  Flex,
+  PasswordInput,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { hasLength, isEmail, useForm } from "@mantine/form";
 
 export default function SignIn() {
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
       email: "",
+      password: "",
+      remember: false,
+    },
+
+    validate: {
+      email: isEmail("Invalid email"),
+      password: hasLength({ min: 8 }),
     },
   });
 
   return (
-    <Card withBorder>
+    <Card withBorder w="350">
+      <Title order={2} mb={16}>
+        Sign In
+      </Title>
+
       <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput label="Email Address" />
+        <Flex direction="column" gap="lg">
+          <Flex direction="column" gap="xs">
+            <TextInput
+              withAsterisk
+              label="Email Address"
+              key={form.key("email")}
+              {...form.getInputProps("email")}
+            />
 
-        <PasswordInput label="Password" />
+            <PasswordInput
+              withAsterisk
+              label="Password"
+              key={form.key("password")}
+              {...form.getInputProps("password")}
+            />
 
-        <Button variant="filled">Login</Button>
+            <Flex justify="space-between" align="center">
+              <Checkbox
+                label="Remember me"
+                key={form.key("remember")}
+                {...form.getInputProps("remember", { type: "checkbox" })}
+              />
+
+              <Button component="a" href="#">Forgot password?</Button>
+            </Flex>
+          </Flex>
+
+          <Button fullWidth type="submit" variant="filled">
+            Login
+          </Button>
+        </Flex>
       </form>
     </Card>
   );

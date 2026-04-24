815a96597712dbf187ee9bd8152d2ec3ac8bfc22
Silas Jimmy <silas.jimmy@softclans.co.ke>
2026-04-24 09:45:20 +0300

    refactor: create dashboard layout

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,9 +1,38 @@
-export default function RootLayout({
+"use client";
+
+import { AppShell, Burger, Group, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <div className="border border-red-500">{children}</div>
+    <AppShell
+      layout="alt"
+      header={{ height: 60 }}
+      navbar={{ width: 250, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          Header
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        <Group>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+
+          <Text>Navbar</Text>
+        </Group>
+      </AppShell.Navbar>
+
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
   );
 }

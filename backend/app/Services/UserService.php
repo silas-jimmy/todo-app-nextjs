025f21da7004ca5f50e58c243bb5aab0795bf8ae
Silas Jimmy <silas.jimmy@softclans.co.ke>
2026-04-29 08:44:18 +0300

    refactor: create login endpoint and logic

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -2,6 +2,10 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class UserService
@@ -16,10 +20,18 @@ class UserService
     public function create(array $data): array
     {
         try {
+            $user = User::create([
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'username' => $data['username'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+
             return [
                 'success' => true,
-                'message' => 'User created successfully',
-                'data' => null
+                'message' => 'User account created successfully',
+                'data' => $user
             ];
         } catch (\Throwable $th) {
             Log::info($th->getMessage());
@@ -42,10 +54,30 @@ class UserService
     public function login(array $data): array
     {
         try {
+            $user = User::firstWhere('email', $data['email']);
+
+            $remember = Arr::get($data, 'remember') ? true : false;
+            $credentials = [
+                'email' => $data['email'],
+                'password' => $data['password']
+            ];
+
+            if (Auth::attempt($credentials, $remember)) {
+                $token = $user->createToken('api token')->plainTextToken;
+                
+                return [
+                    'success' => true,
+                    'message' => 'User logged in successfully',
+                    'data' => $token
+                ];
+            }
+
             return [
-                'success' => true,
-                'message' => 'User logged in successfully',
-                'data' => null
+                'success' => false,
+                'message' => 'Failed to login!',
+                'data' => [
+                    'password' => ['Incorrect password.']
+                ]
             ];
         } catch (\Throwable $th) {
             Log::info($th->getMessage());

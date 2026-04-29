94087e2dea7dc9ab3000e30d3e8ae4f6e1886839
Silas Jimmy <silas.jimmy@softclans.co.ke>
2026-04-29 15:20:42 +0300

    refactor: set up register and login endpoints

---
+++ user-management-service/app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Injects service classes to the controller class.
+     * 
+     * @param UserService $userService the user service class.
+     * 
+     * @return void
+     */
+    public function __construct(
+        protected UserService $userService,
+    ) {}
+
+    /**
+     * Register a new user.
+     * 
+     * @param Request $request the user request object.
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'bail|string|required|max:15',
+            'last_name' => 'bail|string|required|max:15',
+            'username' => 'bail|string|required|unique:users|max:20',
+            'email' => 'bail|string|required|email|unique:users',
+            'password' => 'bail|string|required|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to register!',
+                'data' => $validator->errors(),
+            ], 422);
+        }
+
+        $response = $this->userService->create($request->all());
+
+        if ($response['success']) {
+            return response()->json($response, 201);
+        } else {
+            return response()->json($response, 422);
+        }
+    }
+
+    /**
+     * Logs in a user.
+     * 
+     * @param Request $request the user request object.
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'bail|required|email|exists:users,email',
+            'password' => 'bail|required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to login!',
+                'data' => $validator->errors(),
+            ], 422);
+        }
+
+        $response = $this->userService->login($request->all());
+
+        if ($response['success']) {
+            return response()->json($response, 200);
+        } else {
+            return response()->json($response, 401);
+        }
+    }
+}
--- user-management-service/routes/api.php
+++ user-management-service/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register')->name('register');
+
+    Route::post('/login', 'login')->name('login');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();

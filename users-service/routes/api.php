5ec2bf3ea626573ef5f04d0d8cd4b521d53f164c
Silas Jimmy <silas.jimmy@softclans.co.ke>
2026-05-06 17:22:47 +0300

    refactor: set up password grant in users service passport

--- users-service/routes/api.php
+++ users-service/routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json();
+Route::prefix('users')->group(function () {
+    Route::controller(AuthenticationController::class)->group(function () {
+        Route::post('/register', 'register')->name('register');
+
+        Route::post('/login', 'login')->name('login');
+    });
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
